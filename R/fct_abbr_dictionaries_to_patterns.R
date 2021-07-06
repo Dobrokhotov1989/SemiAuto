4e7125ee50a30bafe172523a4bc9d57211d20a58
Dobrokhotov1989 <45655146+Dobrokhotov1989@users.noreply.github.com>
2021-07-06 10:02:05 +0900

    update

--- R/fct_abbr_dictionaries_to_patterns.R
+++ R/fct_abbr_dictionaries_to_patterns.R
@@ -11,11 +11,13 @@ abbr_dictionaries_to_patterns <- function(files, header){
   
   dictionaries <- purrr::map(files,
                              ~ readr::read_csv(.x, col_names = header))
-  
+
   patterns <- purrr::map_chr(.x = dictionaries,
                              .f = function(x){
                                x %>%
                                  dplyr::pull(1) %>%
+                                 stringr::str_replace_all(pattern = " ",
+                                                          replacement = "\\s?")%>%
                                  stringr::str_c(collapse = "\\b|\\b") %>%
                                  stringr::str_c("\\b", ., "\\b")
                                
